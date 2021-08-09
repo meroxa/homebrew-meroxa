d4dc1187a39452218aae7829b549e7a366acd512
meroxa-machine <machine@meroxa.io>
2021-08-09 16:15:35 -0400

    Brew formula update for meroxa version v1.0.1

--- Formula/meroxa.rb
+++ Formula/meroxa.rb
@@ -5,29 +5,29 @@
 class Meroxa < Formula
   desc "The Meroxa CLI"
   homepage "https://meroxa.io"
-  version "1.0.0"
+  version "1.0.1"
   license "Apache 2.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/meroxa/cli/releases/download/v1.0.0/meroxa_1.0.0_darwin_amd64.tar.gz"
-      sha256 "f2cfae8dd1393a7599d2a111cd272eb6cf63fbb4d33e66e3a3b1e34d5a0bee65"
+      url "https://github.com/meroxa/cli/releases/download/v1.0.1/meroxa_1.0.1_darwin_amd64.tar.gz"
+      sha256 "a77e02f11e7c4403989aab6935b1695a8db6eb9ef09d525e8a8138632737c88c"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/meroxa/cli/releases/download/v1.0.0/meroxa_1.0.0_darwin_arm64.tar.gz"
-      sha256 "d9f20cab0e3e3a8e6686c99ac7382c7fc38c7e9033834c9862e43126fe6da9a5"
+      url "https://github.com/meroxa/cli/releases/download/v1.0.1/meroxa_1.0.1_darwin_arm64.tar.gz"
+      sha256 "cccb890a6b87979997bac7cb2dea069492a2142adfc53022827cb1402f9574ef"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/meroxa/cli/releases/download/v1.0.0/meroxa_1.0.0_linux_amd64.tar.gz"
-      sha256 "ff8a8f929fba21cd113c64b028a6f3bf9723eea424083c9ab0825918afae8836"
+      url "https://github.com/meroxa/cli/releases/download/v1.0.1/meroxa_1.0.1_linux_amd64.tar.gz"
+      sha256 "0f9efcc30b97a8b679af4b27b3ade55433141901f1ddf1c41cdcdbc1c2ce37ee"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/meroxa/cli/releases/download/v1.0.0/meroxa_1.0.0_linux_arm64.tar.gz"
-      sha256 "c9ba0a0b10816371ab1a72e3e39e1e9e416dbf31ee9ab460c251971aa0840d8e"
+      url "https://github.com/meroxa/cli/releases/download/v1.0.1/meroxa_1.0.1_linux_arm64.tar.gz"
+      sha256 "eab949405dab806aaf66d0e9ff354d150a58757cfdf8d02d801adba4f3cdcf3b"
     end
   end
 
@@ -43,6 +43,6 @@ class Meroxa < Formula
   end
 
   test do
-    shell_output("#{bin}/meroxa version").match(/1.0.0/)
+    shell_output("#{bin}/meroxa version").match(/1.0.1/)
   end
 end
